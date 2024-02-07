203c1aba8b64e286283d7d04edecf3530b57ef5a
Alper Sonmez <124396210+aziaatt@users.noreply.github.com>
2024-02-07 11:19:36 +0100

    Ik heb een letter vervangen.

--- RPG Game/obj/Debug/net8.0/RPG Game.AssemblyInfo.cs
+++ RPG Game/obj/Debug/net8.0/RPG Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b5f62ea4f2ff5d179e506db6a59dbc2cbd6388")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
