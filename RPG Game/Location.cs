5d106ff5383501ca63dfd8f3524c93efccaeb3a7
Amzir-A <102997068+Amzir-A@users.noreply.github.com>
2024-02-13 13:30:11 +0100

    Update setup project

---
+++ RPG Game/Location.cs
@@ -0,0 +1,10 @@
+public int ID;
+public string Name;
+public string Description;
+public Quest QuestAvailableHere;
+public Monster MonsterLivingHere;
+
+public Location LocationToNorth;
+public Location LocationToEast;
+public Location LocationToSouth;
+public Location LocationToWest;
--- RPG Game/obj/Debug/net8.0/RPG Game.AssemblyInfo.cs
+++ RPG Game/obj/Debug/net8.0/RPG Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc36e2b2ec4f88cd34b688baef77f5bcefc4cf6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c28c3f5132213fa8f4a283b28769a42dded0c42")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
